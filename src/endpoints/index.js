419b6cd30782a848846eca223d826464d83d4202
elenamlopez <elena.mateos1@gmail.com>
2020-07-14 13:18:39 +0200

    Fix dependecy inyection

--- src/endpoints/index.js
+++ src/endpoints/index.js
@@ -1,21 +1,5 @@
-// postHandlers
-module.exports = ({ axios }) => ({
-  post: async (req, res) => {
-    const { data: users } = await axios.get(
-      'https://jsonplaceholder.typicode.com/users'
-    );
+const posts = require('./posts/index.js');
 
-    const found = users.find(x => x.id === req.body.userId);
-
-    if (found) {
-      const { data } = await axios.post(
-        'https://jsonplaceholder.typicode.com/posts',
-        req.body
-      );
-
-      return res.status(201).send(data);
-    }
-
-    return res.sendStatus(400);
-  },
-});
+module.exports = {
+  posts,
+};
--- src/index.js
+++
@@ -1,6 +0,0 @@
-const posts = require('./endpoints/index.js');
-console.log(posts);
-
-module.exports = {
-  posts,
-};
